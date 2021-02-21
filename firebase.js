6a15080fc4954d80412225d1e52f09b74453cc56
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-02-21 20:38:52 +0530

    commit

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsMo0Wy_8N5OHYI7szaNGSo5gpv-JAk00",
-  authDomain: "signal-clone-4eab1.firebaseapp.com",
-  projectId: "signal-clone-4eab1",
-  storageBucket: "signal-clone-4eab1.appspot.com",
-  messagingSenderId: "1093155981535",
-  appId: "1:1093155981535:web:8f1464312bc606b7c3416a",
-  measurementId: "G-K83N7G3KZB",
+  apiKey: "AIzaSyBxy9Y8Z1-80kSFEHXqUx70N0ZNZMOBIuA",
+  authDomain: "signal-clone-d6ee3.firebaseapp.com",
+  projectId: "signal-clone-d6ee3",
+  storageBucket: "signal-clone-d6ee3.appspot.com",
+  messagingSenderId: "661885969755",
+  appId: "1:661885969755:web:b234c405dac198375e667e",
+  measurementId: "G-0H8GVJXBN5",
 };
 
 let app;
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -11,6 +11,7 @@ const LoginScreen = ({ navigation }) => {
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      console.log(authUser);
       if (authUser) {
         navigation.replace("Home");
       }
@@ -19,7 +20,11 @@ const LoginScreen = ({ navigation }) => {
     return unsubscribe;
   }, []);
 
-  const signIn = () => {};
+  const signIn = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error));
+  };
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="light" />
@@ -44,6 +49,7 @@ const LoginScreen = ({ navigation }) => {
           type="password"
           value={password}
           onChangeText={(text) => setPassword(text)}
+          onSubmitEditing={signIn}
         />
       </View>
 
